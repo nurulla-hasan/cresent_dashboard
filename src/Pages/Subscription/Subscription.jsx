165a04e5af363e634247846cce26536968c7f6c5
Rabeya Akter <rabeyariya251@gmail.com>
2024-12-30 13:07:33 +0600

    implement add subscription and message cost

--- src/Pages/Subscription/Subscription.jsx
+++ src/Pages/Subscription/Subscription.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { FaPlus, FaTrash } from "react-icons/fa";
 import { FaRegPenToSquare } from "react-icons/fa6";
 import AddSubscription from "../../Components/SubscriptionManagement/AddSubscription/AddSubscription";
+import MessageCost from "./MessageCost/MessageCost";
+import EditSubscription from "../../Components/SubscriptionManagement/EditSubscription/EditSubscription";
 
 const Subscription = () => {
   const [showModal, setShowModal] = useState(false);
@@ -36,7 +38,7 @@ const Subscription = () => {
       </div>
       <div className="my-10 grid grid-cols-1 md:grid-cols-3 gap-5">
         <div className="bg-white p-5 rounded-xl border border-primary">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mb-2">
             <h1>Basic Subscription</h1>
             <div className="flex items-center gap-2">
               <button onClick={() => hndleShowEditModal()} >
@@ -48,15 +50,98 @@ const Subscription = () => {
                 <FaTrash className="text-red-500 " /></button>
             </div>
           </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Price</p>
+            <p className="text-primary">90$</p>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Validity</p>
+            <p className="text-primary">1 Year</p>
+          </div>
+          <div>
+            <h1>Feature list</h1>
+            {/* bullet list */}
+            <ul className="list-disc list-inside flex flex-col gap-2  text-neutral-500">
+              <li>up to 50+ appointment</li>
+              <li>Unlimited video cal </li>
+              <li>Unlimited Audio call</li>
+            </ul>
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-xl border border-primary">
+          <div className="flex items-center justify-between mb-2">
+            <h1>Basic Subscription</h1>
+            <div className="flex items-center gap-2">
+              <button onClick={() => hndleShowEditModal()} >
+                <FaRegPenToSquare className="text-primary " />
+              </button>
+
+
+              <button onClick={() => handleDelet()} >
+                <FaTrash className="text-red-500 " /></button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Price</p>
+            <p className="text-primary">90$</p>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Validity</p>
+            <p className="text-primary">1 Year</p>
+          </div>
+          <div>
+            <h1>Feature list</h1>
+            {/* bullet list */}
+            <ul className="list-disc list-inside flex flex-col gap-2  text-neutral-500">
+              <li>up to 50+ appointment</li>
+              <li>Unlimited video cal </li>
+              <li>Unlimited Audio call</li>
+            </ul>
+          </div>
+        </div>
+        <div className="bg-white p-5 rounded-xl border border-primary">
+          <div className="flex items-center justify-between mb-2">
+            <h1>Basic Subscription</h1>
+            <div className="flex items-center gap-2">
+              <button onClick={() => hndleShowEditModal()} >
+                <FaRegPenToSquare className="text-primary " />
+              </button>
+
+
+              <button onClick={() => handleDelet()} >
+                <FaTrash className="text-red-500 " /></button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Price</p>
+            <p className="text-primary">90$</p>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <p>Validity</p>
+            <p className="text-primary">1 Year</p>
+          </div>
+          <div>
+            <h1>Feature list</h1>
+            {/* bullet list */}
+            <ul className="list-disc list-inside flex flex-col gap-2  text-neutral-500">
+              <li>up to 50+ appointment</li>
+              <li>Unlimited video cal </li>
+              <li>Unlimited Audio call</li>
+            </ul>
+          </div>
         </div>
       </div>
+      <MessageCost />
+
+      {/* Add Subscription Modal */}
+      <Modal title="Add Subscription" open={showModal} onCancel={handleCancel} footer={null}>
+        <AddSubscription />
+      </Modal>
 
 
       {/* Add Subscription Modal */}
-
-      <Modal open={showModal} onCancel={handleCancel} footer={null}>
-        <AddSubscription />
-
+      <Modal title="Edit Subscription" open={showEditModal} onCancel={handleCancelEdit} footer={null}>
+        <EditSubscription />
       </Modal>
 
     </div>
