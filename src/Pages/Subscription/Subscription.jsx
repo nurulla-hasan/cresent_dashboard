7941eff978488cc3d3f14cd6abcfdedf5be383f4
RabeyaAkter78 <rabeyariya251@gmail.com>
2025-09-26 15:10:29 +0600

    implement subscriptiona nd payment top section

--- src/Pages/Subscription/Subscription.jsx
+++ src/Pages/Subscription/Subscription.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { message, Modal } from "antd";
 import { useState } from "react";
-import { FaPlus, FaTrash } from "react-icons/fa";
+import { FaArrowDown, FaPlus, FaTrash } from "react-icons/fa";
 import { FaRegPenToSquare } from "react-icons/fa6";
 import AddSubscription from "../../Components/SubscriptionManagement/AddSubscription/AddSubscription";
 import MessageCost from "./MessageCost/MessageCost";
@@ -12,35 +12,33 @@ import DonorsSubscription from "../../Components/ManageSubscription/DonorsSubscr
 const Subscription = () => {
   const [showModal, setShowModal] = useState(false);
 
-
   const handleShowModal = () => {
     setShowModal(true);
-  }
+  };
   const handleCancel = () => {
     setShowModal(false);
-  }
+  };
 
   const handleDelet = () => {
-    message.success('Deleted Successfully');
-  }
+    message.success("Deleted Successfully");
+  };
 
   return (
-    <div>
-      <div className="flex flex-col md:flex-row justify-between md:items-center mb-10">
-        <h3 className="text-xl md:text-2xl font-semibold text-textColor px-2 md:px-0">
-          Subscription
-        </h3>
-        {/* <button onClick={handleShowModal} className="flex items-center gap-2 bg-primary text-center w-full md:w-auto  p-2 font-semibold text-white px-10 py-2 rounded-xl shadow-lg"><FaPlus></FaPlus> Add Subscription</button> */}
-
-      </div>
-      <div>
-
+    <div className="p-6 bg-gray-50 min-h-screen">
+      {/* ---------- Top Title ---------- */}
+      <div className="flex justify-between items-center mb-6">
+        <div>
+          <h1 className="text-2xl font-semibold">Subscription & Payments</h1>
+          <p className="text-neutral-400">
+            Track all subscriptions, manage invoices, and export payment data.
+          </p>
+        </div>
+        <button className="bg-white px-4 py-2 rounded-3xl border flex justify-between items-center gap-2">
+          <FaArrowDown /> Export
+        </button>
       </div>
       <OrganizationSubscription />
       <DonorsSubscription />
-    
-
-
     </div>
   );
 };
