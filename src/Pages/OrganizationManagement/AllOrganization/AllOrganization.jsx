9235ad88bf68dda7294b2a430c2d7c9a1def3279
RabeyaAkter78 <rabeyariya251@gmail.com>
2025-09-27 08:46:40 +0600

    add organization

--- src/Pages/OrganizationManagement/AllOrganization/AllOrganization.jsx
+++ src/Pages/OrganizationManagement/AllOrganization/AllOrganization.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { FaArrowDown } from 'react-icons/fa';
+import React from "react";
+import { FaArrowDown, FaPlus } from "react-icons/fa";
 
 const AllOrganization = () => {
-    return (
-      <div className="p-6 bg-gray-50 min-h-screen">
-           {/* ---------- Top Title ---------- */}
-           <div className="flex justify-between items-center mb-6">
-             <div>
-               <h1 className="text-2xl font-semibold">Subscription & Payments</h1>
-               <p className="text-neutral-400">
-                 Track all subscriptions, manage invoices, and export payment data.
-               </p>
-             </div>
-             <button className="bg-white px-4 py-2 rounded-3xl border flex justify-between items-center gap-2">
-               <FaArrowDown /> Export
-             </button>
-           </div>
-            
+  return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+      {/* ---------- Top Title ---------- */}
+      <div className="flex justify-between items-center mb-6">
+        <div>
+          <h1 className="text-2xl font-semibold">Subscription & Payments</h1>
+          <p className="text-neutral-400">
+            Track all subscriptions, manage invoices, and export payment data.
+          </p>
         </div>
-    );
+        <div className="flex justify-center items-center gap-2">
+          <button className="bg-white px-4 py-2 rounded-3xl border flex justify-between items-center gap-2">
+            <FaPlus /> Add Organization
+          </button>
+          <button className="bg-white px-4 py-2 rounded-3xl border flex justify-between items-center gap-2">
+            <FaArrowDown /> Export
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllOrganization;
