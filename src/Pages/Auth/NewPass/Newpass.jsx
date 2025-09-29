fae7b1133ad9236a16d9db84aac775defbbdf6d2
Rabeya Akter <rabeyariya251@gmail.com>
2025-09-29 10:49:04 +0600

    complete auth

--- src/Pages/Auth/NewPass/Newpass.jsx
+++ src/Pages/Auth/NewPass/Newpass.jsx
@@ -11,109 +11,90 @@ const Newpass = () => {
     neviaget("/");
   };
   return (
-    <div className="">
-      <div className="relative grid grid-cols-1 md:grid-cols-2 justify-center items-center p-4">
-        <div className="bg-white p-10">
-          <img src={logo} alt="Logo" className="absolute top-5" />
-          <div className="w-full max-w-sm mx-auto mt-20">
-            <ConfigProvider
-              theme={{
-                components: {
-                  Form: {
-                    borderRadius: 0,
-                  },
-                  Input: {
-                    borderRadius: 5,
-                  },
+    <div className="h-screen flex p-2">
+      <div className="bg-white flex flex-col justify-center items-center w-full md:w-1/2">
+        <img src={logo} alt="Logo" className="absolute top-5 right-5 left-10" />
+        <div className=" w-[400px] ">
+          <ConfigProvider
+            theme={{
+              components: {
+                Form: {
+                  borderRadius: 0,
                 },
-              }}
+                Input: {
+                  borderRadius: 5,
+                },
+              },
+            }}
+          >
+            <Form
+              name="login"
+              initialValues={{ remember: false }}
+              onFinish={onFinish}
+              layout="vertical"
             >
-              <Form
-                name="login"
-                initialValues={{ remember: false }}
-                style={{ maxWidth: 600 }}
-                onFinish={onFinish}
-                layout="vertical"
+              <div className="mb-4 text-center">
+                <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
+                  Reset Your Password
+                </h2>
+                <p className="text-neutral-400  pt-3 pb-6">
+                  The password must be different than previous password.
+                </p>
+              </div>
+
+              <Form.Item
+                name="new-password"
+                label={<p className="text-lg ">New Password</p>}
               >
-                <div className="mb-4 text-center">
-                  <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
-                    Reset Your Password
-                  </h2>
-                  <p className="text-neutral-600 lg:text-lg pt-3 pb-6"> 
-                    The password must be different than previous password.
-                  </p>
-                </div>
+                <Input.Password
+                  required
+                  className=""
+                  prefix={<MdLockOutline className="mr-2 h-5 w-5" />}
+                  placeholder="********"
+                  style={{
+                    padding: "8px",
+                    borderRadius: "8px",
+                    width: "100%",
+                    height: "52px",
+                  }}
+                />
+              </Form.Item>
+              <Form.Item
+                name="confirm-password"
+                label={
+                  <p className="text-lg ">Confirm Password</p>
+                }
+              >
+                <Input.Password
+                  required
+                  className=""
+                  prefix={<MdLockOutline className="mr-2 h-5 w-5" />}
+                  placeholder="********"
+                  style={{
+                    padding: "8px",
+                    borderRadius: "8px",
+                    width: "100%",
+                    height: "52px",
+                  }}
+                />
+              </Form.Item>
 
-                {/* Email input field */}
-                {/* <Form.Item
-                  name="email"
-                  label={<p className="text-md">Email</p>}
+              <Form.Item>
+                <button
+                  onClick={handleResetPassword}
+                  className=" w-full py-4 font-bold bg-primary rounded-md text-xl"
+                  type="submit"
                 >
-                  <Input
-                    required
-                    className="text-md"
-                    placeholder="Enter Email Address"
-                  />
-                </Form.Item> */}
-
-                {/* Password input field */}
-                <Form.Item
-                  name="new-password"
-                  label={
-                    <p className="text-lg text-neutral-500">New Password</p>
-                  }
-                >
-                  <Input.Password
-                    required
-                    className="text-neutral-500"
-                    prefix={<MdLockOutline className="mr-2 h-5 w-5" />}
-                    placeholder="********"
-                    style={{
-                      padding: "8px",
-                      borderRadius: "8px",
-                      width: "100%",
-                      height: "52px",
-                    }}
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="confirm-password"
-                  label={
-                    <p className="text-lg text-neutral-500">Confirm Password</p>
-                  }
-                >
-                  <Input.Password
-                    required
-                    className="text-neutral-500"
-                    prefix={<MdLockOutline className="mr-2 h-5 w-5" />}
-                    placeholder="********"
-                    style={{
-                      padding: "8px",
-                      borderRadius: "8px",
-                      width: "100%",
-                      height: "52px",
-                    }}
-                  />
-                </Form.Item>
-
-                <Form.Item>
-                  <button
-                    onClick={handleResetPassword}
-                    className=" w-full py-4 font-bold bg-primary rounded-md text-xl"
-                    type="submit"
-                  >
-                    Update Password
-                  </button>
-                </Form.Item>
-              </Form>
-            </ConfigProvider>
-          </div>
+                  Update Password
+                </button>
+              </Form.Item>
+            </Form>
+          </ConfigProvider>
         </div>
+      </div>
 
-        {/* Right section - Image */}
-        <div className="flex justify-end items-end">
-          <img src={img} alt="sign-up" className="w-full h-screen" />
-        </div>
+      <div className="w-full md:w-1/2 ">
+        <img src={img} alt="sign-up" className="w-full h-full" />
       </div>
     </div>
   );
--- src/Pages/Auth/VerifyPass/VerifyPass.jsx
+++ src/Pages/Auth/VerifyPass/VerifyPass.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const VerifyPass = () => {
-  const [otp, setOtp] = useState(""); // initialize as empty string
+  const [otp, setOtp] = useState("");
   const nevigate = useNavigate();
   const handleVerifyOtp = () => {
     console.log("Verifying OTP:", otp);
@@ -14,68 +14,58 @@ const VerifyPass = () => {
 
   const handleResendOtp = () => {
     console.log("Resend OTP clicked");
-    // Add your resend logic here
   };
 
   return (
-    <div className="">
-      <div className="relative grid grid-cols-1 md:grid-cols-2 justify-center items-center p-4">
-        {/* Left section - Login Form */}
-        <div className="bg-white p-10">
-          <img src={logo} alt="Logo" className="absolute top-5" />
+    <div className="h-screen flex p-2">
+      <div className="bg-white flex flex-col justify-center items-center w-full md:w-1/2">
+        <img src={logo} alt="Logo" className="absolute top-5 right-5 left-10" />
 
-          <div className="flex justify-center items-center">
-            <div className="">
-              <h1 className="text-3xl text-center font-bold py-5">
-                Enter Verification Code
-              </h1>
-              <p className="text-center text-gray-500">
-                We’ve sent a verification code to{" "}
-                <span className="text-black">userofficialemail@gmail.com</span>
-              </p>
-              <p className="py-3 font-medium">Enter your verification code</p>
-              <div className="pb-7 pt-2 flex justify-center items-center">
-                <OTPInput
-                  value={otp}
-                  onChange={setOtp}
-                  numInputs={6}
-                  renderSeparator={<span className="lg:w-10"> </span>}
-                  renderInput={(props) => (
-                    <input
-                      {...props}
-                      className="md:w-8 h-12 border border-gray-300 text-black text-xl focus:outline-none focus:border-blue-400 mx-1 rounded-md"
-                    />
-                  )}
+        <div className="">
+          <h1 className="text-3xl text-center font-bold py-5">
+            Enter Verification Code
+          </h1>
+          <p className="text-center text-gray-500">
+            We’ve sent a verification code to{" "}
+            <span className="text-black">userofficialemail@gmail.com</span>
+          </p>
+          <p className="py-3 font-medium">Enter your verification code</p>
+          <div className="pb-7 pt-2 flex justify-center items-center">
+            <OTPInput
+              value={otp}
+              onChange={setOtp}
+              numInputs={6}
+              renderSeparator={<span className="lg:w-10"> </span>}
+              renderInput={(props) => (
+                <input
+                  {...props}
+                  className="md:w-8 h-12 border border-gray-300 text-black text-xl focus:outline-none focus:border-blue-400 mx-1 rounded-md"
                 />
-              </div>
-
-              <button
-                onClick={handleVerifyOtp}
-                className="text-center w-full p-3 font-bold text-xl bg-primary text-black rounded-md "
-              >
-                Verify
-              </button>
-
-              <p className="text-center pt-5">
-                Didn’t receive the code?
-                <span
-                  onClick={handleResendOtp}
-                  className="pl-2 underline cursor-pointer"
-                >
-                  Resend
-                </span>
-              </p>
-            </div>
+              )}
+            />
           </div>
-        </div>
 
-        <div className="flex justify-end items-end">
-          <img
-            src={img}
-            alt="sign-up"
-            className="w-full h-screen object-cover"
-          />
+          <button
+            onClick={handleVerifyOtp}
+            className="text-center w-full p-3 font-bold text-xl bg-primary text-black rounded-md "
+          >
+            Verify
+          </button>
+
+          <p className="text-center pt-5">
+            Didn’t receive the code?
+            <span
+              onClick={handleResendOtp}
+              className="pl-2 underline cursor-pointer"
+            >
+              Resend
+            </span>
+          </p>
         </div>
+      </div>
+
+      <div className="w-full md:w-1/2">
+        <img src={img} alt="sign-up" className="w-full h-full" />
       </div>
     </div>
   );
